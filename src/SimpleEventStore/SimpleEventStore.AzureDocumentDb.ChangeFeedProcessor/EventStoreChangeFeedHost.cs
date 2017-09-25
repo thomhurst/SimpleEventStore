ae40c789b84575ebffe62a7a821388183ce2a6f5
Mike Gore <mikegore1000@msn.com>
2017-09-25 05:07:44 -0400

    Initial skeleton commit for ChangeFeedProcessor support

---
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.ChangeFeedProcessor/EventStoreChangeFeedHost.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.ChangeFeedProcessor;
+
+namespace SimpleEventStore.AzureDocumentDb.ChangeFeedProcessor
+{
+    public class EventStoreChangeFeedHost : IChangeFeedObserver
+    {
+        private readonly Func<IReadOnlyCollection<StorageEvent>, string, Task> onNextEvent;
+        private readonly ISerializationTypeMap typeMap;
+
+        public EventStoreChangeFeedHost(Func<IReadOnlyCollection<StorageEvent>, string, Task> onNextEvent, ISerializationTypeMap typeMap)
+        {
+            this.onNextEvent = onNextEvent;
+            this.typeMap = typeMap;
+        }
+        
+        public Task OpenAsync(ChangeFeedObserverContext context)
+        {
+            // TODO: Add logging
+            return Task.FromResult(0);
+        }
+
+        public Task CloseAsync(ChangeFeedObserverContext context, ChangeFeedObserverCloseReason reason)
+        {
+            // TODO: Add logging
+            return Task.FromResult(0);
+        }
+
+        // TODO: Make the callback return a Task
+        // TODO: Add error handling & logging
+        // TODO: Make skipping dead letters configurable
+        public async Task ProcessChangesAsync(ChangeFeedObserverContext context, IReadOnlyList<Document> docs)
+        {
+            var events = new List<StorageEvent>();
+            
+            foreach (var d in docs)
+            {
+                events.Add(DeserializeEventFromDocument(d));
+            }
+            
+            await onNextEvent(events, context.PartitionKeyRangeId);
+        }
+        
+        private StorageEvent DeserializeEventFromDocument(Document doc)
+        {
+            var docDbStorageEvent = DocumentDbStorageEvent.FromDocument(doc);
+            return docDbStorageEvent.ToStorageEvent(typeMap);
+        }
+    }
+}
---
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.ChangeFeedProcessor/EventStoreChangeFeedHostFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.ChangeFeedProcessor;
+
+namespace SimpleEventStore.AzureDocumentDb.ChangeFeedProcessor
+{
+    public class EventStoreChangeFeedHostFactory : IChangeFeedObserverFactory
+    {
+        private readonly Func<IReadOnlyCollection<StorageEvent>, string, Task> onNextEvent;
+        private readonly ISerializationTypeMap typeMap;
+
+        public EventStoreChangeFeedHostFactory(Func<IReadOnlyCollection<StorageEvent>, string, Task> onNextEvent, ISerializationTypeMap typeMap)
+        {
+            this.onNextEvent = onNextEvent;
+            this.typeMap = typeMap;
+        }
+        
+        public IChangeFeedObserver CreateObserver()
+        {
+            return new EventStoreChangeFeedHost(onNextEvent, typeMap);
+        }
+    }
+}
