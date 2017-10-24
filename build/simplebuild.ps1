b01959374d9c1af8d21273b4dd188631f723416f
Mike Gore <mikegore1000@msn.com>
2017-10-24 06:07:55 +0100

    Converted to PS build script and enhanced build process

---
+++ build/simplebuild.ps1
@@ -0,0 +1,36 @@
+#TODO: Simplify the script by running in a container with full .NET Framework & .NET Core SDKs
+
+#Variables
+$docDbConfig = "../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.Tests/appsettings.json"
+$buildVersion = "1.0.0"
+$uri = "<URI>"
+$authKey = "<AUTHKEY>"
+$consistencyLevel = "<SESSION>"
+
+#Transform config
+Write-Host "Transforming config"
+$config = Get-Content $docDbConfig -raw | ConvertFrom-Json
+$config.Uri = $uri
+$config.AuthKey = $authKey
+$config.ConsistencyLevel = $consistencyLevel
+$config | ConvertTo-Json  | set-content $docDbConfig
+
+#Restore
+Write-Host "Restoring packages"
+dotnet restore "../src/SimpleEventStore/SimpleEventStore.sln"
+
+#Build
+Write-Host "Building"
+dotnet build "../src/SimpleEventStore/SimpleEventStore" -c "Release" -f "netstandard1.6"
+dotnet build "../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb" -c "Release" -f "netstandard1.6"
+
+#Test
+Write-Host "Running unit tests"
+dotnet test "../src/SimpleEventStore/SimpleEventStore.Tests" -c "Release" -f "netcoreapp2.0"
+dotnet test "../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.Tests" -c "Release" -f "netcoreapp2.0"
+
+#Package - will work once TODO point is covered
+#Write-Host "Packaging"
+#mkdir "./nuget" -Force
+#dotnet pack "../src/SimpleEventStore/SimpleEventStore" -c "Release" -o "./nuget" /p:BuildVersion="$buildVersion" --no-build
+#dotnet pack "../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb" -c "Release" -o "./nuget" /p:BuildVersion="$buildVersion" --no-build
