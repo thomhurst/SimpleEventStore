11c1a0f653813173407ff3b16c4d7b2bd787ef34
Mike Gore <mikegore1000@msn.com>
2017-10-29 18:18:41 +0000

    Updated build process so it runs entirely in the Dockerfile

--- dockerbuild/dockerbuild.ps1
+++ dockerbuild/dockerbuild.ps1
@@ -2,13 +2,11 @@ Param(
     [string]$uri,
     [string]$authKey,
     [string]$consistencyLevel,
-    [string]$buildVersion="1.0.0"
+    [string]$buildVersion
 )
 
 docker build -t ses-build -f DockerFile.build ../ `
 --build-arg uri=$uri `
 --build-arg authKey=$authKey `
 --build-arg consistencyLevel=$consistencyLevel `
---build-arg buildVersion=$buildVersion `
---no-cache `
---rm
+--build-arg buildVersion=$buildVersion
