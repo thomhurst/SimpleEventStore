5b4cfe05d203604821077ace3c583ecce7cddf1f
mikegore1000 <mikegore1000@msn.com>
2018-09-14 05:40:14 +0100

    Supports package versioning and stopping build after a failure

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ COSMOS_AUTHKEY=$2
 COSMOS_CONSISTENCYLEVEL=$3
 NUGET_SOURCE=$4
 NUGET_KEY=$5
+BUILD_VERSION=$6
 
 docker build -t simpleeventstore-build . && \
 docker run --rm --name simpleeventstore-build \
@@ -11,4 +12,5 @@ docker run --rm --name simpleeventstore-build \
 -e COSMOS_CONSISTENCYLEVEL="$COSMOS_CONSISTENCYLEVEL" \
 -e NUGET_SOURCE="$NUGET_SOURCE" \
 -e NUGET_KEY="$NUGET_KEY" \
+-e BUILD_VERSION="$BUILD_VERSION" \
 simpleeventstore-build sh /build/build.sh
--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
-dotnet build ../src/SimpleEventStore/SimpleEventStore.sln -c Release
+dotnet build ../src/SimpleEventStore/SimpleEventStore.sln -c Release /p:Version=$BUILD_VERSION && \
 
-dotnet test ../src/SimpleEventStore/SimpleEventStore.Tests/SimpleEventStore.Tests.csproj -c Release --no-build
-dotnet test ../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.Tests/SimpleEventStore.AzureDocumentDb.Tests.csproj
+dotnet test ../src/SimpleEventStore/SimpleEventStore.Tests/SimpleEventStore.Tests.csproj -c Release --no-build && \
+dotnet test ../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.Tests/SimpleEventStore.AzureDocumentDb.Tests.csproj && \
 
-dotnet pack ../src/SimpleEventStore/SimpleEventStore/SimpleEventStore.csproj -c Release -o /packages --no-build
-dotnet pack ../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/SimpleEventStore.AzureDocumentDb.csproj -c Release -o /packages --no-build
+dotnet pack ../src/SimpleEventStore/SimpleEventStore/SimpleEventStore.csproj -c Release /p:Version=$BUILD_VERSION -o /packages --no-build && \
+dotnet pack ../src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/SimpleEventStore.AzureDocumentDb.csproj -c Release /p:Version=$BUILD_VERSION -o /packages --no-build && \
 
 dotnet nuget push /packages/*.nupkg -s $NUGET_SOURCE -k $NUGET_KEY
